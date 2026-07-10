b8515fda09fbf1c44765655c1a0ae10c6179aed8
Jadeth-Denninson <jadethyuhye@gmail.com>
2026-07-09 22:58:37 -0400

    Conexión db 2

--- nuam/settings.py
+++ nuam/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'nuam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nuam-database',
+        'NAME': 'nuamdb',
         'USER': 'admin',
         'PASSWORD': 'Inacap.2026',
         'HOST': 'nuam-database.c6xljfguqnwo.us-east-1.rds.amazonaws.com',
