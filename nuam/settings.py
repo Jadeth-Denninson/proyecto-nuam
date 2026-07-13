aee8532fa059e62c830f0149cc13d952fc130b7b
Jadeth-Denninson <jadethyuhye@gmail.com>
2026-07-13 00:48:07 -0400

    Cambios 9

--- nuam/settings.py
+++ nuam/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-mhpu96_s20nq4vpehhqrb*f-b=*59q-%yf%ju8v87p04*4h6wx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,11 +81,20 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nuamdb',
-        'USER': 'root',
-        'PASSWORD': ''
+        'USER': 'admin',
+        'PASSWORD': 'Inacap.2026', 
+        'HOST': 'nuam-database.c6xljfguqnwo.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
+COSMOS_ENDPOINT = "https://nuam-proyecto.documents.azure.com:443/"
+
+COSMOS_KEY = "nuamjson" 
+
+COSMOS_DATABASE = "Nuam-Cosmos"
+
+COSMOS_CONTAINER = "Container1"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
